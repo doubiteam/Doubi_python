ac3aec62c902b25a55623817fe462ffe30954927
XueHL <380915073@qq.com>
2015-12-23 00:16:11 +0800

    FirstNo

--- Xuehl_homeword/WriteFile.py
+++ Xuehl_homeword/WriteFile.py
@@ -5,8 +5,8 @@ Created on 2015-12-21-
 @author: Xue.HL
 '''
 
-fa = file("C:\\stu\\a.txt", "r+")
-fb = file("C:\\stu\\b.txt", "r+")
+fa = file("..\\a.txt", "r+")
+fb = file("..\\b.txt", "r+")
 fb.writelines(fa.readlines())
 fa.close()
 fb.close()  
